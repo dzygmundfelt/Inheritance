49cf46769f513c9d562397bcb38f7c486ee93390
Dan Zygmund-Felt <danzygmund-felt@zipcoder10.local>
2017-05-02 17:50:15 -0400

    Made my lil Pet app. Feels good to write code that does what you want it to.

---
+++ src/main/java/Pets/BasicPetApp.java
@@ -0,0 +1,39 @@
+package Pets;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BasicPetApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Pet> petList = new ArrayList<Pet>();
+
+        System.out.println("How many pets you got?");
+        int numPets = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < numPets; i++) {
+            Pet pet = new Pet();
+            System.out.println("What kind of pet is pet number " + (i+1) + "?");
+            pet.type = scanner.nextLine();
+            System.out.println("What's your pet's name?");
+            pet.name = scanner.nextLine();
+            petList.add(pet);
+        }
+
+        for(Pet p : petList) {
+            System.out.println(p.petToString());
+        }
+
+    }
+}
+
+class Pet {
+    String type;
+    String name;
+
+    String petToString() {
+        return name + " is a " + type;
+    }
+}
